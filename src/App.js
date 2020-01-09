6882fbf27574af4f69406fad6e6437e4e5ac3116
Janimatti Ellonen <janimatti.ellonen@fraktio.fi>
2020-01-09 10:25:45 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Netlify World! Here we go!</p>
+        <p>Hello Netlify World! Here we go again!</p>
       </header>
     </div>
   );
